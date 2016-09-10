0c84951f0657070d163c504a49ea893d066c5b35
Katie Vogel <katiebvogel@gmail.com>
2016-09-10 15:43:16 -0500

    some modifications

--- public/scripts/CarouselDemoController.js
+++ public/scripts/CarouselDemoController.js
@@ -7,8 +7,8 @@ angular.module('bikeApp').controller('CarouselDemoCtrl', function ($scope) {
     var newWidth = 600 + slides.length + 1;
     slides.push({
       image: 'http://placekitten.com/' + newWidth + '/300',
-      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
-        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+      // text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+      //   ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
     });
   };
   for (var i=0; i<4; i++) {
